efd783bc4e47aa978b037c1d352f476f3f9d8ede
sanblashdzann <115792662+sanblashdzann@users.noreply.github.com>
2023-02-02 15:25:55 +0100

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Esta es una exposición sobre balanceo de carga y escalado automático en AWS');
+  res.end('Exposicion sobre balanceo de carga y escalado automatico en AWS');
 });
 
 server.listen(port, () => {
